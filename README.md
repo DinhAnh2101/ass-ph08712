e1158d788f40ebf1d2f33fc5cd19945835d3572d
DinhAnh2101 <truongdinhanh2101@gmail.com>
2020-03-27 23:21:07 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ass-ph08712
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ass-ph08712)
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogGridComponent } from './blog-grid/blog-grid.component';
+import { SliderComponent } from './slider/slider.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, AboutComponent, BlogComponent, BlogGridComponent, SliderComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
